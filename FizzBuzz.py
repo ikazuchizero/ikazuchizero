d1c913a21c9580041ef1edc1c373c5e0c825afd8
ikazuchizero <90844161+ikazuchizero@users.noreply.github.com>
2022-10-01 21:47:58 +0900

    保守なんて知らん
    なんかめんどかったので完全な処理ではない

---
+++ FizzBuzz.py
@@ -0,0 +1,23 @@
+def isascnum(s):
+  return True if s.isdecimal() and s.isascii() else False
+
+num = input("半角数字を入力>>")
+
+TF = isascnum(num)
+
+if TF == True:
+  num = int(num)
+  if num > 0:
+    text = ""
+    if num % 3 == 0:
+        text += ("Fizz")
+    if num % 5 == 0:
+        text += ("Buzz")
+    if text == "":
+      text = num
+
+    print(text)
+  else:
+    print("無効な入力です")
+else:
+  print("半角数字で入力してください")
